dbf54dcb20c90c23cad8d6e888bfff319b15b3f2
Calebn8010 <calebn8010@gmail.com>
2022-12-05 11:01:31 -0500

    template

---
+++ AutoOats.py
@@ -0,0 +1,20 @@
+import os,sys,random,time,datetime
+
+def wtr_powerOn():
+    print("Powering on")
+
+
+def water_d0():
+    print("Water D0")
+
+def oat_drop():
+    print("Dropping oats")
+    
+
+
+if __name__ == "__main__":
+
+    wtr_powerOn()
+    water_d0()
+    oat_drop()
+    
