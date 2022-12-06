a14c3a6355ce67b0f75179c5f65ee45647aa540c
EliNichols6 <105531174+EliNichols6@users.noreply.github.com>
2022-12-06 15:04:17 -0500

    Update AutoOats.py

--- AutoOats.py
+++ AutoOats.py
@@ -3,7 +3,10 @@ import os,sys,random,time,datetime
 # Monday = 0, Sunday = 6
 def getDate():
     wDay = dt.weekday()
+def getTime():
+    dTime = datetime.now()
 
+dt = datetime.now()
 
 # Function that powers on the water
 def wtr_powerOn():
@@ -30,30 +33,65 @@ def oat_drop(day):
   
 # Function for monday - Executes wtr_powerOn, water_d0, and oat_drop on monday
 def mon():
+    oat_drop(wDay)
+    time.sleep(6)
+    wtr_powerOn()
+    time.sleep(5)
+    water_d0()
+    time.sleep(120)
+    wtr_powerOn()
     print("Executed")
     #sleeps for one day 
     time.sleep(86400)
 
     
 def tue():
+    oat_drop(wDay)
+    time.sleep(6)
+    wtr_powerOn()
+    time.sleep(5)
+    water_d0()
+    time.sleep(120)
+    wtr_powerOn()
     print("Executed")
     #sleeps for one day 
     time.sleep(86400)
 
     
 def wed():
+    oat_drop(wDay)
+    time.sleep(6)
+    wtr_powerOn()
+    time.sleep(5)
+    water_d0()
+    time.sleep(120)
+    wtr_powerOn()
     print("Executed")
     #sleeps for one day 
     time.sleep(86400)
 
     
 def thur():
+    oat_drop(wDay)
+    time.sleep(6)
+    wtr_powerOn()
+    time.sleep(5)
+    water_d0()
+    time.sleep(120)
+    wtr_powerOn()
     print("Executed")
     #sleeps for one day 
     time.sleep(86400)
 
     
 def fri():
+    oat_drop(wDay)
+    time.sleep(6)
+    wtr_powerOn()
+    time.sleep(5)
+    water_d0()
+    time.sleep(120)
+    wtr_powerOn()
     print("Executed")
     #sleeps for three days
     time.sleep(259200)
@@ -61,23 +99,27 @@ def fri():
     
     
 if __name__ == "__main__":
-#sleeps for one day - this is a placeholder
-    time.sleep(86400)
 
-    if wDay == 0:
-        mon()
-    elif wDay == 1:
-        tue()
-    elif wDay == 2:
-        wed()
-    elif wDay == 3:
-        thur()
-    elif wDay == 4:
-        fri()    
+    a = True
+    while a == True:
+        getDate()
+        
+        if wDay == 0:
+            mon()
+        elif wDay == 1:
+            tue()
+        elif wDay == 2:
+            wed()
+        elif wDay == 3:
+            thur()
+        elif wDay == 4:            
+            fri()      
+
+   
     
 
-    wtr_powerOn()
-    water_d0()
-    oat_drop(wDay)
+    #wtr_powerOn()
+    #water_d0()
+    #oat_drop(wDay)
     # Wait 24 hours then repeat above
     
