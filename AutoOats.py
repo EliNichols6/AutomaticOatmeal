29bd9f64c08091499a47d5a18a40d600e857acd5
EliNichols6 <105531174+EliNichols6@users.noreply.github.com>
2022-12-06 14:05:58 -0500

    Update AutoOats.py
    Added functions for days and a function that grabs the weekday

--- AutoOats.py
+++ AutoOats.py
@@ -1,5 +1,11 @@
 import os,sys,random,time,datetime
 
+# Monday = 0, Sunday = 6
+def getDate():
+    wDay = dt.weekday()
+
+
+# Function that powers on the water
 def wtr_powerOn():
     print("Powering on")
 
@@ -9,12 +15,42 @@ def water_d0():
 
 def oat_drop():
     print("Dropping oats")
+  
+# Function for monday - Executes wtr_powerOn, water_d0, and oat_drop on monday
+def mon():
+    print("Executed")
     
-
-
+def tue():
+    print("Executed")
+    
+def wed():
+    print("Executed")
+    
+def thur():
+    print("Executed")
+    
+def fri():
+    print("Executed")
+    
+    
 if __name__ == "__main__":
+#sleeps for one day
+    time.sleep(86400)
+
+    if wDay == 0:
+        mon()
+    elif wDay == 1:
+        tue()
+    elif wDay == 2:
+        wed()
+    elif wDay == 3:
+        thur()
+    elif wDay == 4:
+        fri()    
+    
 
     wtr_powerOn()
     water_d0()
     oat_drop()
+    # Wait 24 hours then repeat above
     
