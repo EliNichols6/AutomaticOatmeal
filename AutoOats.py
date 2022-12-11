eba1890fba9702582c070a54455a0ad07429d751
EliNichols6 <105531174+EliNichols6@users.noreply.github.com>
2022-12-11 15:06:19 -0500

    Update AutoOats.py

--- AutoOats.py
+++ AutoOats.py
@@ -3,15 +3,7 @@ import RPi.GPIO as GPIO
 from datetime import datetime
 from pytz import timezone
 
-
-"""def updateTime(): 
-    hour = datetime.datetime.now(pytz.timezone('US/Eastern')).hour
-    min = datetime.datetime.now(pytz.timezone('US/Eastern')).minute
-
-def getDate():
-    dt = datetime.now()
-    wDay = dt.weekday()"""
-
+# Function for Monday that executes Oats and Water functions - Same for T, W, TH, and F
 def mon():
     GPIO.output(Relay[0], GPIO.LOW)
     time.sleep(5)
@@ -119,26 +111,27 @@ def fri():
     GPIO.output(Relay[5], GPIO.LOW)
     time.sleep(2)
     GPIO.output(Relay[5], GPIO.HIGH)
-
-    
-    
+  
 if __name__ == "__main__":
+    # Establishes Relays 1-8 (1 is 5, 2 is 6, 3 is 13, etc.)
     Relay = [5, 6, 13, 16, 19, 20, 21, 26]
-
+    # Set up Relays, Iterate through each relay to set them up and set output to high
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     for i in range(0,8):
         GPIO.setup(Relay[i], GPIO.OUT)
-        GPIO.output(Relay[i], GPIO.HIGh)
+        GPIO.output(Relay[i], GPIO.HIGH)
         
     a = True
     while a == True:
+        # Check Date and Time for EST
         tz = timezone('EST')
         datetime.now(tz) 
         hour = datetime.now(tz).hour
         minute = datetime.now(tz).minute
         dt = datetime.now()
-        wDay = dt.weekday()         
+        wDay = dt.weekday()
+        # Checks Day, Time, and Minute to execute daily function
         if wDay == 0 and hour == 6 and minute == 5:
             mon()
             time.sleep(86160)
